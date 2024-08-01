a3c25e6c955c3cadb00fd68c02d66c8836cf2dff
AagamanKc <aagamannkc@gmail.com>
2024-08-01 22:13:13 +0545

    complete web app that let you upload your document and store in sqlite database

--- webapp/File_management/home/views.py
+++ webapp/File_management/home/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import *
 # Create your views here.
+
+
+def home(request):
+    if request.method == "POST":
+        data = request.POST
+        name = data.get('title')
+        description = data.get('description')
+        myfile = request.FILES
+        image = myfile.get('image')
+        text_file = myfile.get('text_file')
+        pdf_file = myfile.get('pdf_file')
+        Document.objects.create(title=name,
+            description=description,
+            image=image,
+            text_file=text_file,
+            pdf_file=pdf_file)
+        return redirect('/')
+    else:
+        documents = Document.objects.all()
+    return render(request, "home.html",{'documents': documents})
